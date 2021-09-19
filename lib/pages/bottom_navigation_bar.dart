9fcb3c880a1e5e0412d84d77868cd875db5681e6
Mohammad Rahmani <rto1680@gmail.com>
2021-09-19 18:37:10 +0430

    Add bottom navigation bar class

---
+++ lib/pages/bottom_navigation_bar.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatefulWidget {
+  const CustomBottomNavigationBar({ Key? key }) : super(key: key);
+
+  @override
+  _CustomBottomNavigationBarState createState() => _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+
+  List<Widget> tabs = [
+    Center(child: Text("Home",style: TextStyle(color: Colors.white))),
+    Center(child: Text("Search",style: TextStyle(color: Colors.white))),
+    Center(child: Text("Profile",style: TextStyle(color: Colors.white))),
+    Center(child: Text("Notification",style: TextStyle(color: Colors.white))),
+    Center(child: Text("Add item",style: TextStyle(color: Colors.white))),
+  ];
+
+  int currentPage = 0;
+
+  setPage(index) {
+    setState(() {
+      currentPage = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade800,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.grey.shade900,
+        title: Text('Navigation Bar'),
+      ),
+      body: tabs[currentPage],
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue.shade400,
+        onPressed: (){
+          setPage(4);
+        },
+        child: Icon(Icons.add, color: Colors.white, size: 30),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.grey.shade900,
+        shape: CircularNotchedRectangle(),
+        child: Container(
+          height: 80,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: Icon(Icons.home, 
+                  color: currentPage == 0 ? Colors.white : Colors.grey, 
+                  size: 30,),
+                onPressed: () {
+                  setPage(0);
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.search, 
+                color: currentPage == 1 ? Colors.white : Colors.grey,
+                size: 30,),
+                onPressed: (){
+                  setPage(1);
+                }
+              ),
+              SizedBox.shrink(),
+              IconButton(
+                icon: Icon(Icons.person, 
+                color: currentPage == 2 ? Colors.white : Colors.grey, 
+                size: 30),
+                onPressed: (){
+                  setPage(2);
+                }
+              ),
+              IconButton(
+                icon: Icon(Icons.notifications_outlined, 
+                color: currentPage == 3 ? Colors.white : Colors.grey, 
+                size: 30),
+                onPressed: (){
+                  setPage(3);
+                }
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
