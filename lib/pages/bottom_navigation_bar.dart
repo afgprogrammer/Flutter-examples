b53ca3bb3346b923efcc8cdf2a466b5968568a35
Mohammad Rahmani <rto1680@gmail.com>
2021-10-05 17:18:02 +0430

    Clean code

--- lib/pages/bottom_navigation_bar.dart
+++ lib/pages/bottom_navigation_bar.dart
@@ -1,3 +1,4 @@
+import 'package:flashy_tab_bar/flashy_tab_bar.dart';
 import 'package:flutter/material.dart';
 
 class CustomBottomNavigationBar extends StatefulWidget {
@@ -37,10 +38,8 @@ class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
       body: tabs[currentPage],
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue.shade400,
-        onPressed: (){
-          setPage(4);
-        },
-        child: Icon(Icons.add, color: Colors.white, size: 30),
+        onPressed: () => setPage(4),
+        child: Icon(Icons.add, color: Colors.white, size: 30,),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
@@ -53,42 +52,59 @@ class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               IconButton(
-                icon: Icon(Icons.home, 
-                  color: currentPage == 0 ? Colors.white : Colors.grey, 
-                  size: 30,),
-                onPressed: () {
-                  setPage(0);
-                },
+                icon: Icon(Icons.home, color: currentPage == 0 ? Colors.white :
+                Colors.grey, size: 30,),
+                onPressed: () => setPage(0),
               ),
               IconButton(
-                icon: Icon(Icons.search, 
-                color: currentPage == 1 ? Colors.white : Colors.grey,
-                size: 30,),
-                onPressed: (){
-                  setPage(1);
-                }
+                icon: Icon(Icons.search, color: currentPage == 0 ? Colors.white :
+                Colors.grey, size: 30,),
+                onPressed: () => setPage(1),
               ),
               SizedBox.shrink(),
               IconButton(
-                icon: Icon(Icons.person, 
-                color: currentPage == 2 ? Colors.white : Colors.grey, 
-                size: 30),
-                onPressed: (){
-                  setPage(2);
-                }
+                icon: Icon(Icons.person, color: currentPage == 0 ? Colors.white :
+                Colors.grey, size: 30,),
+                onPressed: () => setPage(2),
               ),
               IconButton(
-                icon: Icon(Icons.notifications_outlined, 
-                color: currentPage == 3 ? Colors.white : Colors.grey, 
-                size: 30),
-                onPressed: (){
-                  setPage(3);
-                }
-              ),
+                icon: Icon(Icons.notifications_outlined, color: currentPage == 0 ? Colors.white :
+                Colors.grey, size: 30,),
+                onPressed: () => setPage(3),
+              )
             ],
-          ),
-        ),
+          )
+        )
       ),
+      // bottomNavigationBar: FlashyTabBar(
+      //   selectedIndex: currentPage,
+      //   showElevation: true,
+      //   onItemSelected: (index) => setState(() {
+      //     currentPage = index;
+      //   }),
+      //   items: [
+      //     FlashyTabBarItem(
+      //       icon: Icon(Icons.event),
+      //       title: Text('Events'),
+      //     ),
+      //     FlashyTabBarItem(
+      //       icon: Icon(Icons.search),
+      //       title: Text('Search'),
+      //     ),
+      //     FlashyTabBarItem(
+      //       icon: Icon(Icons.highlight),
+      //       title: Text('Highlights'),
+      //     ),
+      //     FlashyTabBarItem(
+      //       icon: Icon(Icons.settings),
+      //       title: Text('Settings'),
+      //     ),
+      //     FlashyTabBarItem(
+      //       icon: Icon(Icons.settings),
+      //       title: Text('한국어'),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
